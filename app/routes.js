ccb2acd2dba5c36f70c38c13889724554d066633
Anamaria-Florentina Titeche <anamaria.titeche@gmail.com>
2026-03-23 19:44:09 +0200

    add new routes

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,14 @@
-import { index } from "@react-router/dev/routes";
+import { index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.jsx")];
+export default [
+  index("routes/index.jsx"),
+
+  /* Authentication */
+  route("/login", "routes/login.jsx"),
+  route("/register", "routes/register.jsx"),
+
+  /* Functionality */
+  route("/dashboard", "routes/dashboard.jsx"),
+  route("/appointments", "routes/appointments.jsx"),
+  route("/profile", "routes/profile.jsx"),
+];
