4269292b1171d14ec40885251697e5dacbeeafac
Anamaria-Florentina Titeche <anamaria.titeche@gmail.com>
2026-04-15 22:32:38 +0300

    remove `dashboard` route and component

--- app/routes.js
+++ app/routes.js
@@ -3,10 +3,7 @@ import { index, prefix, route } from "@react-router/dev/routes";
 export default [
   index("pages/index.jsx"),
 
-  /* Basic functionality */
-  route("/dashboard", "pages/dashboard.jsx"),
-
-  /* A logged in user can access only their own profile and appointments */
+  /* Only a logged in user can access thiese routes */
   route("/profile", "pages/profile.jsx"),
   route("/appointments", "pages/appointments.jsx"),
   route("/ai-scan", "pages/ai-scan.jsx"),
