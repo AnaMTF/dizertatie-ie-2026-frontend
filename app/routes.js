aa51cfafd2036425903eb563d7a8b3b0f18acebd
Anamaria-Florentina Titeche <anamaria.titeche@gmail.com>
2026-04-15 21:31:57 +0300

    simplify routes

--- app/routes.js
+++ app/routes.js
@@ -1,26 +1,13 @@
 import { index, prefix, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/index.jsx"),
-
-  /* Authentication */
-  ...prefix("/auth", [
-    route("/login", "routes/auth/login.jsx"),
-    route("/register", "routes/auth/register.jsx"),
-  ]),
+  index("pages/index.jsx"),
 
   /* Basic functionality */
-  route("/dashboard", "routes/dashboard.jsx"),
+  route("/dashboard", "pages/dashboard.jsx"),
 
   /* A logged in user can access only their own profile and appointments */
-  ...prefix("/profile", [
-    index("routes/profile/profile.jsx"),
-    route("/edit", "routes/profile/profile-edit.jsx"),
-  ]),
-  ...prefix("/appointments", [
-    index("routes/appointments/appointments.jsx"),
-    route("/create", "routes/appointments/appointment-create.jsx"),
-    route("/:uuid", "routes/appointments/appointment.jsx"),
-    route("/:uuid/edit", "routes/appointments/appointment-edit.jsx"),
-  ]),
+  route("/profile", "pages/profile.jsx"),
+  route("/appointments", "pages/appointments.jsx"),
+  route("/ai-scan", "pages/ai-scan.jsx"),
 ];
